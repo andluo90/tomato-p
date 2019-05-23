4ad514630896be8fb15714f2e3558b31b49fca6a
andluo <1455234749@qq.com>
2019-05-23 17:09:23 +0800

    修改history的basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {Router, Route} from "react-router-dom";
+import {createBrowserHistory} from 'history'
 
 import {Provider} from 'react-redux'
 import store from './redux/store'
@@ -10,15 +11,20 @@ import Home from './components/home/Home'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
 
+const history = createBrowserHistory({basename: 'tomato-p'});
+
 class App extends React.Component {
   public render() {
 
     return (
       <Provider store={store}>
-          <Router>
-            <Route exact={true} path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
+          <Router history={history}>
+              <div>
+                  <Route exact={true} path="/" component={Home} />
+                  <Route path="/login" component={Login} />
+                  <Route path="/register" component={Register} />
+              </div>
+              
           </Router>
           
       </Provider>

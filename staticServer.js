10872e5f9813d3688a2b392c9f522e2b86a7ee89
andluo <1455234749@qq.com>
2019-05-31 12:16:50 +0800

    添加error handle

--- staticServer.js
+++ staticServer.js
@@ -8,4 +8,20 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 app.listen(4001);

2f162330855ea432966810c2fa88d8322330f0ee
andluo <1455234749@qq.com>
2019-05-31 12:46:49 +0800

    添加监听端口

--- staticServer.js
+++ staticServer.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.argv[2] || 4113 //默认9000端口
+
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
@@ -24,4 +27,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(4001);
+console.log(`listen at : ${port}`)
+app.listen(port);
